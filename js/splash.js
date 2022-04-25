770a5fe85bd9568c3ca888c8fd64bef58666dcb3
erikmach <e.vf.w00@gmail.com>
2022-04-26 03:06:43 +1000

    splash functions

---
+++ js/splash.js
@@ -0,0 +1,13 @@
+const splash = () => {
+  const splash = document.getElementsByClassName("splash")[0];
+  setTimeout(() => {
+    splash.style.opacity = 0;
+      document.body.style.overflowY = "scroll";
+    setTimeout(() => {
+      document.getElementsByClassName("title")[0].style.zIndex = 0;
+      splash.style.display = "none";
+    }, 400);
+  }, 1000);
+};
+
+window.addEventListener("load", splash, {once: true});
